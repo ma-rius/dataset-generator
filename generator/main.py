8fe29831ef05b54729ec30bace2cc433db4b67b7
Marius Mühlthaler <marius.muehlthaler@student.kit.edu>
2017-09-27 15:56:10 +0200

    safety first

--- generator/main.py
+++ generator/main.py
@@ -6,11 +6,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 from random import randint, random
 from openpyxl import Workbook
+import csv
 
 # -------- Parameters --------
 m = 10  # number of features
 n = 1000  # number of instances
 b = 0.5  # the desired complexity, defined by the length of the class boundary, b ∈[0,1].
+l = [0, 1]  # manually define a set of possible labels
 # -------- End Parameters --------
 
 # TODO switch to csv
@@ -24,11 +26,17 @@ for i in range(1, m + 1, 1):
 
 for i in range(0, m, 1):
     # set distribution for this feature
-    mu, sigma = random() * 100, random() * 100
+    mu, sigma = random() * 100, random() * 100  # TODO distribution for mu and sigma itself ?
+    print('Mu: %dm, Sigma: %s' % (mu, sigma))
     s = np.random.normal(mu, sigma, n)
     print(s)
     print()
+    with open('some.csv', 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerows(np.array(s))
     for j in range(0, n, 1):
         excel_sheet.cell(column=i + 1, row=j + 2, value=int(s[j]))
 
-    wb.save(filename='../assets/Data.xlsx')
+
+    # wb.save(filename='../assets/Data.xlsx')
+
