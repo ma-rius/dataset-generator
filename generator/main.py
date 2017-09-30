74d9c3367d1aae14613d50d38c11f6a31208dac9
Marius Mühlthaler <marius.muehlthaler@student.kit.edu>
2017-09-30 20:44:57 +0200

    bruteforce approach

--- generator/main.py
+++ generator/main.py
@@ -1,18 +1,45 @@
-# TODO only create file with unlabeled and normally distributed instances ones
-
 from scipy.sparse.csgraph import minimum_spanning_tree
 import numpy as np
 from random import randint, random
 import csv
 import scipy.spatial.distance as dist
+from itertools import product
+
+# set print options for large arrays
+np.set_printoptions(threshold=np.inf, precision=2, linewidth=np.inf)
+
 
 # -------- Parameters --------
 m = 5  # number of features
 n = 10  # number of instances
-b = 0.5  # the desired complexity, defined by the length of the class boundary, b ∈[0,1].
+b = 9 / n  # the desired complexity, defined by the length of the class boundary, b ∈[0,1].
 l = [0, 1]  # manually define a set of possible labels
 # -------- End Parameters --------
 
+
+possible_labels = list(product(l, repeat=n))
+
+
+#  assign labels and calculate class boundary
+def get_labels(k):
+    _labels = {}
+    for x in range(0, n, 1):
+        _labels.update({x: possible_labels[k][x]})
+    return _labels
+
+
+# calculate number of edges connecting points of opposite classes is counted and divided by the
+# total number of connections. This ratio is taken as the measure of boundary length
+def class_boundary(_minimum_spanning_tree, _labels):
+    boundary_length = 0
+    for s in range(0, n, 1):
+        for t in range(s + 1, n, 1):
+            if (_minimum_spanning_tree[s][t] != 0) and (_labels[s] != _labels[t]):
+                boundary_length += 1
+    return boundary_length / n
+
+
+# create dataset without labeling
 distribution_dict = {}
 instances = {}
 
@@ -30,12 +57,11 @@ for i in range(0, n, 1):
 
 # store data in csv file
 with open('../assets/data.csv', 'w') as f:
-    wtr = csv.writer(f, delimiter=';')  # Semicolon as delimiter
+    wtr = csv.writer(f, delimiter=',')  # TODO Semicolon as delimiter
     for index in instances:
         wtr.writerow(instances[index])
 
 # build graph
-np.set_printoptions(threshold=np.inf, precision=2, linewidth=np.inf)
 graph = []
 for p in range(0, n, 1):
     row = []
@@ -47,29 +73,25 @@ for p in range(0, n, 1):
         row.append(d)
     graph.append(row)
 
+# convert to numpy array
 graph = np.array(graph)
 print(graph)
 print()
+
+# calculate Minimum Spanning Tree
 # noinspection PyTypeChecker
 mst = minimum_spanning_tree(graph).toarray()
 print(mst)
 
-# assign labels and calculate class boundary
-labels = {}
-for i in range(0, n, 1):
-    labels.update({i: randint(0, 1)})
+num = 0
+labels = get_labels(num)
 
+# while class_boundary(mst, labels) != b:
+while num < 1024:
+    labels = get_labels(num)
+    num += 1
+    print(class_boundary(mst, labels))
+    print(labels.values())
+    print()
 
-# calculate number of edges connecting points of opposite classes is counted and divided by the
-# total number of connections. This ratio is taken as the measure of boundary length
-# If there are n vertices in the graph, then each spanning tree has n − 1 edges
-# -> number of connection = n -1
-def class_boundary(minimum_spanning_tree, labels):
-    boundary_length = 0
-    for s in range(0, n, 1):
-        for t in range(s + 1, n, 1):
-            if (minimum_spanning_tree[s][t] != 0) and (labels[s] != labels[t]):
-                boundary_length += 1
-    return boundary_length / (n - 1)
-
-print(class_boundary(mst, labels))
+# print(class_boundary(mst, labels))
