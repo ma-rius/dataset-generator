83e6a4a3e40b70fd781ad7e12020a4751c8f18de
Marius Mühlthaler <marius.muehlthaler@student.kit.edu>
2017-10-10 23:28:53 +0200

    Logging, comments and code style

--- generator/main.py
+++ generator/main.py
@@ -8,12 +8,13 @@ from deap import creator, base, tools
 from scipy.sparse.csgraph import minimum_spanning_tree
 from scipy.spatial import distance
 from scipy import random
+import multiprocessing
 
 # -------- Dataset Parameters --------
-m = 10  # number of attributes
+m = 200  # number of attributes
 m_groups = 2  # number of independent groups the attributes are divided by
-n = 5  # number of instances
-b = 0.9  # the desired complexity, defined by the length of the class boundary, b ∈[0,1].
+n = 100  # number of instances
+b = 0.2  # the desired complexity, defined by the length of the class boundary, b ∈[0,1].
 # -------- End Dataset Parameters --------
 
 # check if attributes can be divided into specified amount of groups
@@ -22,7 +23,7 @@ if m % m_groups != 0:
 m_per_group = int(m / m_groups)
 
 # -------- GA Parameters --------
-population_size = 1  # int(n / 10)
+population_size = 100  # int(n / 10)
 num_of_generations = 100
 # -------- End GA Parameters --------
 
@@ -31,14 +32,17 @@ num_of_generations = 100
 np.set_printoptions(threshold=np.inf, precision=2, linewidth=np.inf)
 pd.set_option('expand_frame_repr', False)
 
+
+# ----  Dataset Creation ----
 start = time.time()
+
 # initialize mean vectors
 all_means = []
 for g_mean in range(m_groups):
     mean = np.random.randint(100, size=m_per_group)
     all_means.append(mean)
-
 # print('Mean Vectors: \n', all_means)
+print('Mean Vector created.')
 
 # initialize covariance matrices (must be positive semi-definite)
 all_cov = []
@@ -46,48 +50,43 @@ for g_cov in range(m_groups):
     A = random.rand(m_per_group, m_per_group)
     cov = np.dot(A, A.transpose())
     all_cov.append(cov)
-
 # print(all_cov)
+print('Covariance Matrix created.')
 
-# create data
+# get values that follow the specified distribution
 data = pd.DataFrame()
 for group in range(m_groups):
     data = pd.concat([data, pd.DataFrame(np.random.multivariate_normal(all_means[group], all_cov[group], n))], axis=1,
                      ignore_index=True)
 
+# add empty column for the labels
 data['label'] = np.nan
 
-# print(data)
-
+# save in csv file
+print('Store numbers in csv file...')
 data.to_csv(path_or_buf='../assets/data.csv', sep=';', header=data[:].columns.values.tolist(), index=False, decimal=',')
 
-# print('Data Creation and Saving Time:', time.time() - start)
+
+# ----  End of Dataset Creation ----
+print('Data Creation done in:', time.time() - start, 'seconds.')
+
 
 # build distance matrix
-# start = time.time()
+start = time.time()
+print('Calculate Distance Matrix...')
 dist = np.triu(distance.cdist(data[data[:].columns.difference(['label'])], data[data[:].columns.difference(['label'])],
                               'euclidean'))
-# print('Distance Matrix Calculation Time:', time.time() - start)
-# print()
-# print(dist)
+print('Distance Matrix calculated in', time.time() - start, 'seconds.')
+
 
 # calculate Minimum Spanning Tree
-# start = time.time()
+start = time.time()
+print('Calculate MST...')
 mst = minimum_spanning_tree(dist, overwrite=True).toarray()
-# print('MST Calculation Time:', time.time() - start)
-print(mst)
-# print(data['label'])
+print('MST calculated in', time.time() - start, 'seconds.')
+
 
 # ---- GA ----
-creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
-creator.create("Individual", list, fitness=creator.FitnessMin)
-
-toolbox = base.Toolbox()
-toolbox.register("gene", np.random.randint, 0, 2)  # randomly either 0 or 1
-toolbox.register("individual", tools.initRepeat, creator.Individual,
-                 toolbox.gene, n=n)
-toolbox.register("population", tools.initRepeat, list, toolbox.individual)
-
 
 # fitness function
 def evaluate(individual):
@@ -103,6 +102,14 @@ def evaluate(individual):
     return fitness,
 
 
+creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
+creator.create("Individual", list, fitness=creator.FitnessMin)
+
+toolbox = base.Toolbox()
+toolbox.register("gene", np.random.randint, 0, 2)  # randomly either 0 or 1
+toolbox.register("individual", tools.initRepeat, creator.Individual,
+                 toolbox.gene, n=n)
+toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 toolbox.register("mate", tools.cxTwoPoint)
 toolbox.register("mutate", tools.mutFlipBit, indpb=1 / n)
 toolbox.register("select", tools.selTournament, tournsize=3)
@@ -110,26 +117,35 @@ toolbox.register("evaluate", evaluate)
 
 
 def main():
+    print('GA started...')
     pop = toolbox.population(n=population_size)
     # print(pop)
     # ind1 = toolbox.individual()
     # print(ind1)
     # print(evaluate(ind1))
+    hof = tools.HallOfFame(1)
     stats = tools.Statistics(lambda ind: ind.fitness.values)
     stats.register("avg", np.mean)
     stats.register("std", np.std)
     stats.register("min", np.min)
     stats.register("max", np.max)
 
-    pop, log = algorithms.eaSimple(pop, toolbox, cxpb=0.5, mutpb=0.2, ngen=num_of_generations, stats=stats, verbose=1)
+    algorithms.eaSimple(pop, toolbox, cxpb=0.5, mutpb=0.2, ngen=num_of_generations, stats=stats, halloffame=hof,
+                        verbose=True)
 
-    print('Population:', pop)
+    # print('Population:', pop)
     # print(log)
-
-    # data['label'] = pop
-    # data.to_csv('../assets/data_with_labels.csv', index=False)
+    print('------------------------------------')
+    print('Best individual:', hof[0])
+    print('Fitness of best individual:', evaluate(hof[0]))
+    print('------------------------------------')
+    data['label'] = hof[0]
+    data.to_csv('../assets/data.csv', index=False)
 
 
 if __name__ == '__main__':
+    pool = multiprocessing.Pool()
+    toolbox.register("map", pool.map)
     main()
+
     # ---- End GA ----
