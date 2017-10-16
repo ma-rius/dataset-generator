1a522d5f2e074c1d8cf16a19b98d560a70e46a58
Marius Mühlthaler <marius.muehlthaler@student.kit.edu>
2017-10-16 17:09:27 +0200

    version that counts the number of edges

--- generator/main.py
+++ generator/main.py
@@ -3,11 +3,10 @@ import multiprocessing
 import numpy
 from deap import base
 from deap import creator
-
 from generator.functions import *
 
 # -------- Dataset Parameters --------
-n = 100
+n = 10
 m = 15
 
 # -------- GA Parameters --------
@@ -18,9 +17,13 @@ MAX_STRATEGY = 1
 population_size = 100
 
 # -------- Run Parameters --------
-# complexity_measures = [0.1]
-complexity_measures = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
-amount_of_datasets_per_complexity_measure = 2
+complexity_measures = [0.1]
+# complexity_measures = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+amount_of_datasets_per_complexity_measure = 1
+
+# set print options for large arrays
+np.set_printoptions(threshold=np.inf, precision=2, linewidth=np.inf)
+pd.set_option('expand_frame_repr', False)
 
 # initialize GA
 creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
@@ -38,6 +41,7 @@ def main(mst_edges, b, path):
     toolbox.register("mate", tools.cxTwoPoint)
     toolbox.register("mutate", tools.mutFlipBit, indpb=1 / n)
     toolbox.register("select", tools.selTournament, tournsize=3)
+    # toolbox.register("select", tools.selNSGA2)
     toolbox.register("evaluate", evaluate, mst_edges=mst_edges, n=n, b=b)
 
     toolbox.decorate("mate", checkStrategy(MIN_STRATEGY))
@@ -53,29 +57,31 @@ def main(mst_edges, b, path):
     stats.register("min", numpy.min)
     stats.register("max", numpy.max)
 
-    eaSimple(pop, toolbox=toolbox, cxpb=0.9, mutpb=0.4, stats=stats,
+    eaSimple(pop, toolbox=toolbox, cxpb=0.8, mutpb=0.4, stats=stats,
              halloffame=hof,
              verbose=True)
-    # print('Best individual:', hof[0])
+    print('Best individual:', hof[0])
     data = pd.read_csv(path, sep=';', decimal=',')
     data['label'] = [int(x) for x in hof[0]]
-    data.to_csv(path, index=False)
+    data.to_csv(path, index=False)  # TODO sep & decimal
 
     print('Time for this complexity:', time.time() - start_main)
     print('--------------------\n')
 
 
 if __name__ == "__main__":
-    start_ = time.time()
+    start_total = time.time()
     pool = multiprocessing.Pool()
     toolbox.register("map", pool.map)
     for i in range(amount_of_datasets_per_complexity_measure):
+        start_iter = time.time()
         print('\n-------------------- ITERATION %r --------------------\n' % (i+1))
         for complexity in complexity_measures:
             print('Complexity: %r\n' % complexity)
             main(create_dataset(n=n, m=m, covariance_between_attributes=False,
                                 path='../assets/data_%r.csv' % complexity), b=complexity,
                  path='../assets/data_%r.csv' % complexity)
+        print('Time for iteration', (i+1), ':', time.time()-start_iter)
 
-    print('-------------------------------------------------')
-    print('Total time:', time.time() - start_)
+    print('\n-------------------------------------------------')
+    print('Total time:', time.time() - start_total)
