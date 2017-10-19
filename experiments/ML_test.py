0145959154a140e976e8a562f588fc4037d8b695
Marius Mühlthaler <marius.muehlthaler@student.kit.edu>
2017-10-19 16:31:28 +0200

    boxplot for multiple data sets

--- experiments/ML_test.py
+++ experiments/ML_test.py
@@ -1,7 +1,9 @@
 from numpy import genfromtxt
+import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 from sklearn.svm import LinearSVC
+from sklearn.svm import SVC
 from sklearn.neural_network import MLPClassifier
 from sklearn import ensemble
 from sklearn.model_selection import train_test_split
@@ -9,38 +11,64 @@ from sklearn.datasets import make_classification
 from sklearn import metrics
 from sklearn.model_selection import GridSearchCV
 
-X1, y1 = make_classification(n_features=4, random_state=0)
+# set print options for large arrays
+np.set_printoptions(threshold=np.inf, precision=2, linewidth=np.inf)
+pd.set_option('expand_frame_repr', False)
+
+complexity_measures = [0.2, 0.4, 0.6, 0.8]
+num_data_sets = 50
+
 # define number of attributes in Test set
 num_attributes = 5
 
-# import assets
-# last column is target
-df = pd.read_csv('../assets/data_0.2.csv', sep=',', header=0)
+data_to_plot = []
 
-# prepare data for scikit learn
-y = np.array(df['label'])
-X = np.array(df[df.columns[0:num_attributes-1]])
+for complexity in complexity_measures:
+    all_results_for_one_complexity = []
+    for i in range(num_data_sets):
+        # import assets
+        # last column is target
+        df = pd.read_csv('../assets/complexity_%r/data_%r.csv' % (complexity, (i+1)), sep=',', header=0)
 
-# perform train/test split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
-X_sub, X_meta, y_sub, y_meta = train_test_split(X, y, test_size=0.33, random_state=42)
+        # prepare data for scikit learn
+        y = np.array(df['label'])
+        X = np.array(df[df.columns[0:num_attributes]])
+        # print(X)
 
-# define clf
-# clf = MLPClassifier(solver=‘lbfgs’, alpha=1e-5, hidden_layer_sizes=(15,), random_state=1)
-clf = ensemble.GradientBoostingClassifier()
-# clf = LinearSVC()
-# parameters = {'loss':('deviance', 'exponential'), 'learning_rate':[0.1, 0.3,0.4,0.7,0.9]}
-# clf = GridSearchCV(ensemble.GradientBoostingClassifier(), parameters, verbose=10)
+        # perform train/test split
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+        X_sub, X_meta, y_sub, y_meta = train_test_split(X, y, test_size=0.33, random_state=42)
 
-# start clf training
-clf.fit(X_train, y_train)
+        # define clf
+        # clf = MLPClassifier(solver=‘lbfgs’, alpha=1e-5, hidden_layer_sizes=(15,), random_state=1)
+        # clf = ensemble.GradientBoostingClassifier()
+        # clf = SVC()
+        parameters = {'loss':('deviance', 'exponential'), 'learning_rate':[0.1, 0.3,0.4,0.7,0.9]}
+        # svc_parameters = {'C': [0.001, 1, 10, 100], 'kernel':['linear', 'poly']}
+        clf = GridSearchCV(ensemble.GradientBoostingClassifier(), parameters, verbose=10)
+        # clf = GridSearchCV(SVC(), svc_parameters, verbose=10, n_jobs=-1)
 
-# predict test values
-y_pred = clf.predict(X_test)
+        # start clf training
+        clf.fit(X_train, y_train)
 
-# calculate metrics
-results = metrics.classification_report(y_test, y_pred)
-print(metrics.accuracy_score(y_test, y_pred))
+        # predict test values
+        y_pred = clf.predict(X_test)
 
-print(results)
-print(metrics.confusion_matrix(y_test, y_pred))
+        # calculate metrics
+        results = metrics.classification_report(y_test, y_pred)
+        print(metrics.accuracy_score(y_test, y_pred))
+
+        print(results)
+        print(metrics.confusion_matrix(y_test, y_pred))
+
+        all_results_for_one_complexity.append(metrics.f1_score(y_test, y_pred))
+
+    data_to_plot.append(all_results_for_one_complexity)
+
+# fig = plt.figure()
+# ax = fig.add_subplot(111)
+# ax.boxplot(data_to_plot, patch_artist=True)
+
+plt.boxplot(data_to_plot, positions=complexity_measures)
+# plt.xticks(complexity_measures)
+plt.show()
